6a9269c51f9ea15114072de3baf1b9247dd62c89
Nelson Cheng <nelsoncheng@mac.com>
2015-03-12 14:09:22 +0800

    Submission of exercise 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Exercise 2
+##
+## The following functions creates a 'special matrix' which caches the inverse matrix. This allows speedy return of
+## the inverse if it has already been calculated. It assumes the underlying matrix is not singular.
 
-## Write a short comment describing this function
+## makecacheMatrix() creates a vector object that wraps an underlying matrix.
+## The vector contains the functions to get/set the matirx and that for its inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    myInv <- NULL
+  
+    set <- function(y) {
+        x <<- y
+        myInv <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinv <- function(inv) myInv <<- inv
+    
+    getinv <- function() myInv
+    
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() takes a special matrix and returns its inverse. It will perform
+## the calculation if it is not cached.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+  
+    inv <- x$getinv()
+    if (!is.null(inv)) {
+         message("getting cached data")
+         return(inv)
+    }
+    inv <- solve(x$get())
+    x$setinv(inv)
+    inv
 }
